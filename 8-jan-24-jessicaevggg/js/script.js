7c5149dfaf358018606a877c98a0a0ecf56eaefe
jessicaevggg <155990294+jessicaevggg@users.noreply.github.com>
2024-01-17 16:12:00 +0700

    Add files via upload

---
+++ 8-jan-24-jessicaevggg/js/script.js
@@ -0,0 +1,20 @@
+function luas(){
+    var s = document.getElementById('s').value;
+
+    var luas = s*s;
+
+    document.getElementById('luas').innerHTML = luas;
+}
+
+function kel(){
+    var s = document.getElementById('s').value;
+
+    var kel = 4*s;
+
+    document.getElementById('kel').innerHTML = kel;
+}
+
+function resetButton(){
+    document.getElementById("myForm").reset();
+
+}
